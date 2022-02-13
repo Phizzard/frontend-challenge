ee4069ee5938d7e9adbca53b78e9cabc6adbf738
Phil Tietjen <tietjen.philip@gmail.com>
2022-02-13 01:20:24 -0500

    feat(components): add Container

---
+++ src/components/Container.jsx
@@ -0,0 +1,8 @@
+export function Container({ children }) {
+  return (
+    <div className="max-w-lg mx-auto px-4 sm:px-6 lg:px-8">
+      {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+      <div className="max-w-3xl mx-auto">{children}</div>
+    </div>
+  );
+}
