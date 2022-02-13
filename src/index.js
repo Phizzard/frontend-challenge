dd827758d5600869a6feb5f26696a5fc1fcf60ee
Phil Tietjen <tietjen.philip@gmail.com>
2022-02-13 01:17:47 -0500

    chore: setup project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import App from "./App";
+import "./tailwind.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
