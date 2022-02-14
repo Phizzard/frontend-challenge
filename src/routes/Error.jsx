7d82a1da1e1a7c97d17102f57d45038e23e44ef2
Phil Tietjen <tietjen.philip@gmail.com>
2022-02-14 00:09:05 -0500

    feat(error): special message

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -4,9 +4,11 @@ import { Button } from "components/Button";
 import { Container } from "components/Container";
 import { Card, CardActions } from "components/Card";
 import { PageTitle } from "components/PageTitle";
+import { useLocalStorage } from "hooks/useLocalStorage";
 
 export default function Error() {
   const navigate = useNavigate();
+  const [signUpData] = useLocalStorage("sign_up_form", {});
 
   const onRestart = () => {
     navigate("/", { replace: true });
@@ -17,8 +19,12 @@ export default function Error() {
       <PageTitle>Error</PageTitle>
       <Card>
         <div className="space-y-4">
-          <XCircleIcon className="text-red-500 h-24 mx-auto" />
-          <PageTitle className="font-normal">Uh oh. Something went wrong. Please try again later</PageTitle>
+          <XCircleIcon className="dark:text-red-400 text-red-500 h-24 mx-auto" />
+          <PageTitle className="font-normal">
+            {signUpData?.name === "Error"
+              ? "Uh oh. You used the name 'Error' didn't you? Please try again now"
+              : "Uh oh. Something went wrong. Please try again later"}
+          </PageTitle>
         </div>
         <CardActions>
           <Button className="w-full sm:w-auto" onClick={onRestart}>
